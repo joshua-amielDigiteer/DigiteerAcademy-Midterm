cc5c9241eb2ede9c2c79fd49479472112710ed83
joshua-amielDigiteer <joshuadavocol.digiteeracademy@gmail.com>
2023-10-23 13:53:26 +0800

    Installed devise

--- app/models/product_sku.rb
+++ app/models/product_sku.rb
@@ -1,5 +1,5 @@
 class ProductSku < ApplicationRecord
     belongs_to :product
     validates :code, presence: true,  uniqueness: true
-    validates :color, presence: true, uniqueness: true
+    validates :color, presence: true, uniqueness: {scope: :product_id}
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.6/src/index.js"
 pin "@checkbox", to: "https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js", preload: true
+pin "@tooltip", to: "https://unpkg.com/@material-tailwind/html@latest/scripts/tooltip.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
